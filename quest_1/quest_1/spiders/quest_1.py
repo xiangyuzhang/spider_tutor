3769462c96d062e86b104911d3f4c9b6eaa40884
xiangyu <xiangyuzhang365@gmail.com>
2016-01-14 15:27:39 -0500

    quest1 完成
    在parse里面 用 yield Request可以跟进link

--- quest_1/quest_1/spiders/quest_1.py
+++ quest_1/quest_1/spiders/quest_1.py
@@ -4,7 +4,7 @@ __author__ = 'parallels'
 import scrapy
 import scrapy.log
 from bs4 import BeautifulSoup
-import scrapy.log
+from scrapy.http import Request
 from items import Item
 from scrapy.selector import Selector
 import re
@@ -14,19 +14,24 @@ class heibanke1(scrapy.Spider):
 #   allowd_domains = ["http://www.heibanke.com"]
 #    allowed_domains = ["dmoz.org"]
     start_urls = ["http://www.heibanke.com/lesson/crawler_ex00/"]
-    scrapy.log.msg("here we enter the class")
+
 
 
     def parse(self, response):
 
-        scrapy.log.msg("enter parse")
-
         key = response.xpath("//html/body/div/div/div/h3").re(r'([0-9]+)')[1]
-#         print key
-
-        with open("key","wb") as f:
-            f.write(key)
-        return {"key":key}
+        if(len(key) >= 2):
+            print "find new url, continuing..."
+            new_url = "http://www.heibanke.com/lesson/crawler_ex00/" + key
+            with open("key","a") as f:
+                f.write(key)
+            yield Request(url = new_url, callback=self.parse)
+        else:
+            print "new mission found!!!Good Job!!!\nNew Url has been save in 'New Mission'"
+            new_mission = response.xpath("//div/a").re("\"/.*/\"")[-1]
+            print new_mission
+            with open("new Mission",'a') as f:
+                f.write(new_mission)
 #        with open("body", 'wb') as f:
 #            f.write(response.body)
 
