35af37bd81ad8e4a033ee8c295d844f597c9eff5
xiangyu <xiangyuzhang365@gmail.com>
2016-01-19 16:13:33 -0500

    quest2完成了，不能循环在于不能调用build_in函数start_requests,但是为什么呢

--- quest_2/quest_2/spiders/heibanke2.py
+++ quest_2/quest_2/spiders/heibanke2.py
@@ -11,25 +11,37 @@ def start_requests(usrname, password):
 class heibanke2(scrapy.Spider):
     name = "herbanke2"
 #     start_urls = ["http://www.heibanke.com/lesson/crawler_ex01/"]
-    password = 0
+    password = 4
 
     def start_requests(self):
+        return [Request("http://www.heibanke.com/lesson/crawler_ex01/", callback = self.post_login,dont_filter = True)]
+
+    def start_request(self):
         return [Request("http://www.heibanke.com/lesson/crawler_ex01/", callback = self.post_login,dont_filter = True)]
 
     #FormRequeset
     def post_login(self, response):
         print 'Preparing login'
-
+        print "current password:" , str(self.password)
         return [scrapy.FormRequest.from_response(response,
                             formdata = {
-                            'usrname': 'Joselyn',
+                            'username': "JoseLyn",
                             'password': str(self.password)
                             },
                             callback = self.after_login
                             )]
     def after_login(self,response):
         print "after_login"
-        self.start_requests()
+        with open("body" + str(self.password),"wb") as f:
+            f.write(response.body)
+        soup = BeautifulSoup(response.body,"lxml")
+        if "JoseLyn" not in soup.h3.string:
+            self.password += 1
+            return [Request("http://www.heibanke.com/lesson/crawler_ex01/", callback = self.post_login,dont_filter = True)]
+
+        else:
+            print "password found:", str(self.password)
+            print "next mission at:", 'http://www.heibanke.com' + soup.a['href']
 #        soup = BeautifulSoup(response.body,"lxml")
 #        with open("body","wb") as f:
 #            f.write(response.body)
